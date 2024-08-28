ee8bdf03db142d4e7de68e18b2916df8064eccb8
ofc <ofc@gov.cn>
2024-08-28 18:38:40 +0800

    RSA

--- msign.py
+++ msign.py
@@ -3,10 +3,15 @@
 """
 Module implementing common.
 """
+
 from Crypto.PublicKey import RSA
 from Crypto.PublicKey import ECC
 from Crypto.Signature import DSS
 from Crypto.Hash import SHA224, SHA256, SHA384, SHA512
+from Crypto.Cipher import PKCS1_OAEP
+# from Crypto.Cipher import PKCS1_v1_5
+# from Crypto.Random import get_random_bytes
+import sys
 
 class Msign:
     """
@@ -16,24 +21,102 @@ class Msign:
     @classmethod
     def rsa_key_gen(cls):
         # random_generator = Random.new().read
-        rsa = RSA.generate(2048)
+        rsa_key = RSA.generate(2048)
         # n (integer) – RSA modulus
-        print('n:', rsa.n)
+        print('n:', rsa_key.n)
         # e (integer) – RSA public exponent
-        print('e:', rsa.e)
+        print('e:', rsa_key.e)
         # d (integer) – RSA private exponent
-        print('d:', rsa.d)
+        print('d:', rsa_key.d)
         # p (integer) – First factor of the RSA modulus
-        print('p:', rsa.p)
+        print('p:', rsa_key.p)
         # q (integer) – Second factor of the RSA modulus
-        print('q:', rsa.q)
+        print('q:', rsa_key.q)
+        print('dp:', rsa_key.dp)
+        print('dq:', rsa_key.dq)
+        print('invp:', rsa_key.invp)
+        print('invq:', rsa_key.invq)
         # 生成私钥
-        print(rsa.export_key(format='DER'))
-        private_key = rsa.export_key()
-        print(private_key.decode('utf-8'))
+        # print(rsa_key.export_key(format='DER'))
+        private_key = rsa_key
+        print(private_key.export_key().decode('utf-8'))
         # 生成公钥
-        public_key = rsa.publickey().export_key()
-        print(public_key.decode('utf-8'))
+        public_key = rsa_key.publickey()
+        print(public_key.export_key().decode('utf-8'))
+        return public_key, private_key
+
+    @classmethod
+    def rsa_public_key_encrypt(cls, publicKey, message):
+        print('{}[{}]>>>>>>>'.format(sys._getframe().f_code.co_name, sys._getframe().f_lineno))
+        cipher = PKCS1_OAEP.new(publicKey)
+        # cipher = PKCS1_v1_5.new(publicKey)
+        cryptiontext = cipher.encrypt(message)
+        print(cryptiontext.hex())
+        return cryptiontext
+
+    @classmethod
+    def rsa_private_key_decrypt(cls, privateKey, message):
+        print('{}[{}]>>>>>>>'.format(sys._getframe().f_code.co_name, sys._getframe().f_lineno))
+        cipher = PKCS1_OAEP.new(privateKey)
+        cryptiontext = cipher.decrypt(message)
+        # cipher = PKCS1_v1_5.new(privateKey)
+        # sentinel = get_random_bytes(16)
+        # cryptiontext = cipher.decrypt(message, sentinel)
+        print(cryptiontext.hex())
+        return cryptiontext
+
+    @classmethod
+    def rsa_public_components_encrypt(cls, n, e, message):
+        print('{}[{}]>>>>>>>'.format(sys._getframe().f_code.co_name, sys._getframe().f_lineno))
+        publicKey = RSA.construct((n, e))
+        print('n:', hex(publicKey.n))
+        print('e:', hex(publicKey.e))
+        cipher = PKCS1_OAEP.new(publicKey)
+        cryptiontext = cipher.encrypt(message)
+        print(cryptiontext.hex())
+        return cryptiontext
+
+    @classmethod
+    def rsa_private_components_decrypt(cls, n, e, d, message):
+        print('{}[{}]>>>>>>>'.format(sys._getframe().f_code.co_name, sys._getframe().f_lineno))
+        privateKey = RSA.construct((n, e, d))
+        print('n:', hex(privateKey.n))
+        print('e:', hex(privateKey.e))
+        print('d:', hex(privateKey.d))
+        cipher = PKCS1_OAEP.new(privateKey)
+        cryptiontext = cipher.decrypt(message)
+        # cipher = PKCS1_v1_5.new(privateKey)
+        # sentinel = get_random_bytes(16)
+        # cryptiontext = cipher.decrypt(message, sentinel)
+        print(cryptiontext.hex())
+        return cryptiontext
+
+    @classmethod
+    def rsa_test_case(cls):
+        public_key, private_key = Msign.rsa_key_gen()
+        message = b'\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A\x31\x32\x33\x34\x35\x36'
+
+        ciphertext = Msign.rsa_public_key_encrypt(public_key, message)
+        plaintext = Msign.rsa_private_key_decrypt(private_key, ciphertext)
+        if message == plaintext:
+            print('RSA test ok!')
+        else:
+            print('RSA test failed!')
+
+        n = b'\xDA\x07\x50\x17\xDB\x2E\xE5\xEF\xCD\x3D\x86\xE2\x1D\x42\x9D\xC4\xB3\x5D\xD4\xB8\xC7\x31\xEA\xEC\x83\x01\xBF\xCD\xF4\xBB\x76\xFB\x89\xF0\xFA\x48\x5A\x23\xAB\xB6\xE3\x9F\xBB\x4A\x92\x87\x6C\x5E\xA1\x93\x0A\x56\x6D\x51\xC4\x13\x4C\x26\x34\x9C\xEE\x7A\x68\x10\x93\xED\x69\xD4\x4E\x64\xF8\xBC\x3C\x83\xE9\x87\x28\xEE\x36\x10\x09\x87\x87\xBD\x3C\x53\xDA\xE1\x73\xC6\xCF\xCC\x6F\x52\xFE\x30\x77\x62\xCA\x71\xAD\x80\x5E\x79\xDD\x06\xB7\xDD\x2F\x5B\x91\xD9\x9C\x04\x7C\x07\xE4\x8D\x00\xAA\x4A\xF9\xCC\x78\x20\x7D\xF7\x1D\x7A\xB3\x56\xF8\x5C\xD1\x83\x81\x23\x03\xE9\x4F\x2C\xAD\xFE\x47\x24\x52\x53\x8B\x48\xCE\xF1\x8C\xE3\xF2\x94\xA0\xB7\x5E\x5B\x86\x84\xD6\xCD\x83\x02\x38\xA1\x16\xFC\x56\x2D\xA4\x54\x1E\xFB\x5D\x37\x94\x7C\xFD\x2E\x77\x23\x74\x9E\x47\x1D\xAE\xA9\x01\x57\x63\x81\xC2\x9B\x00\x64\xD3\xBE\xB1\x40\x23\x2A\x47\x47\x49\xFB\xFD\x91\x57\x87\x81\x85\x92\x1E\x1D\x85\xF7\x3C\x1F\x71\x5D\x3A\xA8\x32\x8B\x2D\x78\x64\xE3\x33\x2A\xFB\x6C\x2C\xA0\xFA\xA0\x6F\xF3\x7B\xA4\x15\x95\x79\xC0\xBB\x2A\x4A\xFB\x0A\x6E\x39\x18\xC4\x11'
+        e = b'\x00\x01\x00\x01'
+        d = b'\xD3\xD1\x42\xF8\xCA\x52\x77\xC5\x4F\x8A\x34\xDE\xBC\x3A\x99\xF8\x1D\xA2\x4D\x25\xEF\x30\x09\xE2\x19\x2C\xBB\xE1\x25\xA0\x72\xE9\xD1\x8B\xB2\x3C\x54\x86\x4F\x2E\xF1\x59\x9F\xA5\xC5\x51\x97\xF1\x1F\xDC\x30\xB0\xE2\xA8\x0B\x6C\x0C\x11\x1A\x10\x54\x87\xCB\x4E\x3F\xC8\xE9\x85\xFF\x5B\x8D\x63\xA3\x0F\x2C\xF3\xC8\xCA\x14\xEF\xE3\xB2\x2E\x27\xA4\xD2\x7A\xD5\x89\x9B\x5F\x0A\x68\x25\x23\x1A\x87\x02\x0D\x57\xFF\x8D\xB5\x3B\x83\x86\x21\x5E\xDB\xC1\x84\xA2\xFB\x36\x48\x21\x8A\xE3\x89\xFF\xE7\xB3\x63\xF8\x58\xE8\xC8\x07\xA9\x75\x5F\x83\x5E\x54\x1D\x65\x5A\xB4\xEC\xC0\x51\x6C\x85\xAC\xD0\x2E\xF1\x8F\x84\x18\xAE\xAB\x5A\x03\xBC\x6B\x80\x85\x22\x26\xD8\xE5\xEA\xA2\x63\x53\x1C\x03\xD8\x35\x0D\x63\x6E\x73\xF7\x18\x21\x39\x2E\xB7\x5F\x74\x62\x7D\xAF\x1F\x98\x28\x75\x21\x5E\x07\xF0\x94\x5D\x02\x85\xF9\x8B\x3F\xA6\xEE\x42\x55\x1E\x49\xE0\x40\xFB\x22\x27\xE0\x61\xBD\xD9\xD4\xAF\x31\x97\xCA\x45\x13\x1A\xD7\x25\xE8\xCD\x12\x4E\x65\x80\xEC\x2A\x7C\x5C\xD9\x6A\xDA\x9B\x64\x67\x07\x60\x85\xCE\xB5\xB9\x4E\x9B\xB0\x09\xAC\xF5\xB7\x56\x19'
+        message = b'\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A\x31\x32\x33\x34\x35\x36'
+
+        ciphertext = Msign.rsa_public_components_encrypt(int.from_bytes(n, 'big'), int.from_bytes(e, 'big'),
+                                               message)
+        plaintext = Msign.rsa_private_components_decrypt(int.from_bytes(n, 'big'), int.from_bytes(e, 'big'),
+                                               int.from_bytes(d, 'big'), ciphertext)
+        
+        if message == plaintext:
+            print('RSA test ok!')
+        else:
+            print('RSA test failed!')
 
     @classmethod
     def ecc_key_gen(cls, curve_bits):
@@ -163,4 +246,4 @@ class Msign:
 
 if __name__ == '__main__':
     # print(sys.argv)
-    Msign.ecc_test_case()
+    Msign.rsa_test_case()
